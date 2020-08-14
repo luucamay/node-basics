831b24ac8c110077cb75cadad07a0109b098a761
Imilla Dev <lupemaydanda@gmail.com>
2020-08-13 22:48:26 -0400

    feat: Play with console methods

---
+++ modulos/consola.js
@@ -0,0 +1,12 @@
+console.group('conversacion');
+console.log('Hola');
+console.group('respuesta');
+console.log('na nana');
+console.log('bla bla');
+console.groupEnd('respuesta');
+console.log('bla bla bla blah');
+console.groupEnd('conversacion');
+console.log('algo')
+
+console.count('peras');
+console.count('peras');
