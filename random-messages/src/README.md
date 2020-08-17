e2545bd7d8d5f2d63ac43ab81863fe6bb054d87c
Imilla Dev <lupemaydanda@gmail.com>
2020-08-16 23:03:23 -0400

    feat: Create a hello world npm package

--- random-messages/src/README.md
+++
@@ -1,6 +0,0 @@
-# Node Basics
-## Troubleshoting
-if ```npm link`` does not work use ```npm pack```
-
-## Resources
-- [Test packages before publishing](https://medium.com/@vcarl/problems-with-npm-link-and-an-alternative-4dbdd3e66811)
--- random-messages/src/index.js
+++
@@ -1,8 +0,0 @@
-const fruits = ['pera', 'manzana', 'frutilla', 'naranja'];
-
-const randomFruit = () => {
-  const fruit = fruits[Math.floor(Math.random()* fruits.length)];
-  console.log(fruit);
-};
-
-module.exports = { randomFruit };
